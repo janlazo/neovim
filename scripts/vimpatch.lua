4fa6b5a4cc0e5309affb2ad2e18602b24c8002a6
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2025-11-20 01:29:39 -0500

    feat(version): support multiple Vim versions
    Group up to 15 vimpatch numbers in 1 line to guard against
    'make formatc'.
    1-liner for vim_versions, num_patches.
    
    Automate '*Version' to remove version.h macros.

--- scripts/vimpatch.lua
+++ scripts/vimpatch.lua
@@ -20,57 +20,98 @@ local function vimpatch_sh_list_tokens()
 end
 
 -- Generates the lines to be inserted into the src/nvim/version.c
--- `included_patches[]` definition.
+-- to populate the following:
+-- - `Version`
+-- - `vim_versions[]`
+-- - `num_patches[]`
+-- - `included_patchsets[]`
 local function gen_version_c_lines()
   -- Sets of merged Vim x.y.zzzz patch numbers.
   local merged_patch_sets = {}
+  local vim_version_str
   for _, token in ipairs(vimpatch_sh_list_tokens()) do
     local major_version, minor_version, patch_num = string.match(token, '^(%d+).(%d+).(%d+)$')
     local n = tonumber(patch_num)
-    -- TODO(@janlazo): Allow multiple Vim versions
-    -- if n then
-    if n and major_version == '8' and minor_version == '1' then
+    if n then
       local major_minor_version = major_version * 100 + minor_version
+      if vim_version_str == nil then
+        vim_version_str = major_version .. '.' .. minor_version
+      end
       merged_patch_sets[major_minor_version] = merged_patch_sets[major_minor_version] or {}
       table.insert(merged_patch_sets[major_minor_version], n)
     end
   end
 
-  local sorted_versions = {}
-  for k, _ in pairs(merged_patch_sets) do
-    table.insert(sorted_versions, k)
-  end
-  table.sort(sorted_versions)
-  local lines = {}
-  for _, major_minor_version in ipairs(sorted_versions) do
-    -- table.insert(lines, string.format('  // major minor version: %s', major_minor_version))
-    local patch_set = merged_patch_sets[major_minor_version]
+  local major_vim_versions = {}
+  local num_patches = {}
+  local patch_lines = {}
+  for major_minor_version, patch_set in vim.spairs(merged_patch_sets) do
+    table.insert(major_vim_versions, major_minor_version)
+    table.insert(num_patches, #patch_set)
+    table.insert(patch_lines, '  (const int[]) {  // ' .. major_minor_version)
+
+    local patchset_set = {}
     for i = #patch_set, 1, -1 do
       local patch = patch_set[i]
-      table.insert(lines, string.format('  %s,', patch))
-      if patch > 0 then
-        local oldest_unmerged_patch = patch_set[i - 1] and (patch_set[i - 1] + 1) or 0
-        for unmerged_patch = patch -1, oldest_unmerged_patch, -1 do
-          table.insert(lines, string.format('  // %s,', unmerged_patch))
+      local next_patch = patch_set[i - 1]
+      local patch_diff = patch - (next_patch or 0)
+      table.insert(patchset_set, patch)
+
+      -- guard against last patch or `make formatc`
+      if #patchset_set > 15 or i == 1 or patch_diff > 1 then
+        table.insert(patch_lines, '    ' .. table.concat(patchset_set, ', ') .. ',')
+        patchset_set = {}
+      end
+      if i == 1 and patch > 0 then
+        local line = '    // 0'
+        if patch > 1 then
+          line = line .. '-' .. (patch - 1)
         end
+        table.insert(patch_lines, line)
+      elseif patch_diff > 1 then
+        local line = '    // ' .. (next_patch + 1)
+        if patch_diff > 2 then
+          line = line .. '-' .. (patch - 1)
+        end
+        table.insert(patch_lines, line)
       end
     end
+
+    table.insert(patch_lines, '  },')
   end
 
-  return lines
+  return vim_version_str, major_vim_versions, num_patches, patch_lines
 end
 
 local function patch_version_c()
-  local lines = gen_version_c_lines()
+  local vim_version_str, major_vim_versions, num_patches, patch_lines = gen_version_c_lines()
 
   nvim.nvim_command('silent noswapfile noautocmd edit src/nvim/version.c')
-  nvim.nvim_command('/static const int included_patches')
+  nvim.nvim_command([[/^char \*Version =]])
+  -- Replace the line.
+  nvim.nvim_call_function('setline', {
+    nvim.nvim_eval('line(".")'),
+    'char *Version = "' .. vim_version_str .. '";',
+  })
+  nvim.nvim_command([[/^static const int vim_versions]])
+  -- Replace the line.
+  nvim.nvim_call_function('setline', {
+    nvim.nvim_eval('line(".")'),
+    'static const int vim_versions[] = { ' .. table.concat(major_vim_versions, ', ') .. ' };',
+  })
+  nvim.nvim_command([[/^static const int num_patches]])
+  -- Replace the line.
+  nvim.nvim_call_function('setline', {
+    nvim.nvim_eval('line(".")'),
+    'static const int num_patches[] = { ' .. table.concat(num_patches, ', ') .. ' };',
+  })
+  nvim.nvim_command([[/^static const int \*included_patchsets]])
   -- Delete the existing lines.
   nvim.nvim_command('silent normal! j0d/};\rk')
   -- Insert the lines.
   nvim.nvim_call_function('append', {
     nvim.nvim_eval('line(".")'),
-    lines,
+    patch_lines,
   })
   nvim.nvim_command('silent write')
 end
--- src/nvim/version.h
+++ src/nvim/version.h
@@ -11,20 +11,4 @@ extern char *longVersion;
 extern char *version_cflags;
 #endif
 
-//
-// Vim version number, name, etc. Patchlevel is defined in version.c.
-//
-
-// Values that change for a new release
-#define VIM_VERSION_MAJOR                8
-#define VIM_VERSION_MINOR                1
-
-// Values based on the above
-#define VIM_VERSION_MAJOR_STR STR(VIM_VERSION_MAJOR)
-#define VIM_VERSION_MINOR_STR STR(VIM_VERSION_MINOR)
-#define VIM_VERSION_100     (VIM_VERSION_MAJOR * 100 + VIM_VERSION_MINOR)
-
-// swap file compatibility (max. length is 6 chars)
-#define VIM_VERSION_SHORT VIM_VERSION_MAJOR_STR "." VIM_VERSION_MINOR_STR
-
 #include "version.h.generated.h"
